cbd84a21c912443a02211a7290dd6dadd09abb0a
mdmarek <marek@lytics.io>
2017-01-19 15:20:49 -0800

    Package documentation.

--- retry.go
+++ retry.go
@@ -1,3 +1,22 @@
+// Package retry retries something X number of times, with an exponential backoff
+// between each attempt. The backoff is calculated to reach the maximum backoff
+// within three attempts.
+// 
+// Example:
+//     // Retry six times with a maximum backoff of 5 seconds
+//     // between the retry attempts. The maximum backoff is
+//     // reached within the third attempt, since retry uses
+//     // exponential backoff.
+//     var err error
+//     retry.X(6, 5*time.Second, func() bool {
+//         err = DoSomething()
+//         return err != nil
+//     })
+//     if err != nil {
+//         // The error is not nil, so all retries failed.
+//     } else {
+//         // The error is nil, so one succeeded.
+//     }
 package retry
 
 import (
